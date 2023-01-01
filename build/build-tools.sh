058f7464a4fa3bbed2810165fd30a6aeb92f508d
Brian W Bush <code@functionally.io>
2023-01-01 06:04:40 -0700

    Updated environment variable names.

--- build/build-tools.sh
+++ build/build-tools.sh
@@ -8,6 +8,6 @@ nix build -o ./cardano-address ../repos/cardano-addresses
 nix build -o ./cardano-wallet  ../repos/cardano-wallet
 nix build -o ./cardano-db-sync ../repos/cardano-db-sync
 
-ls -lh .
-
-ls -lh ../bin
+nix-build -o ./uplc ../repos/plutus -A plutus.haskell.projectPackages.plutus-core.components.exes.uplc
+nix-build -o ./plc  ../repos/plutus -A plutus.haskell.projectPackages.plutus-core.components.exes.plc
+nix-build -o ./pir  ../repos/plutus -A plutus.haskell.projectPackages.plutus-core.components.exes.pir
--- repos/pull-and-build.sh
+++ repos/pull-and-build.sh
@@ -13,3 +13,8 @@ nix build .#marlowe-history -o ../../build/marlowe-history
 nix build .#marlowe-discovery -o ../../build/marlowe-discovery
 nix build .#marlowe-tx -o ../../build/marlowe-tx
 nix build .#marlowe-rt -o ../../build/marlowe
+
+nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-scaling -o ../../build/marlowe-scaling
+nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-oracle -o ../../build/marlowe-oracle
+nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-finder -o ../../build/marlowe-finder
+
