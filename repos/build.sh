14becc7b42126ba4553813e30fec084f60f9e871
Brian W Bush <code@functionally.io>
2023-02-05 17:00:20 -0700

    Updated build scripts.

--- repos/build.sh
+++ repos/build.sh
@@ -4,9 +4,6 @@ set -ev
 
 cd marlowe-cardano
 
-git fetch
-git rebase origin/main
-
 nix build .#chainseekd -o ../../build/chainseekd
 nix build .#marlowe-chain-indexer -o ../../build/marlowe-chain-indexer
 nix build .#marlowe-indexer -o ../../build/marlowe-indexer
@@ -15,7 +12,6 @@ nix build .#marlowe-history -o ../../build/marlowe-history
 nix build .#marlowe-discovery -o ../../build/marlowe-discovery
 nix build .#marlowe-tx -o ../../build/marlowe-tx
 nix build .#marlowe-rt -o ../../build/marlowe
-nix build .#marlowe-cli -o ../../build/marlowe-cli
 
 nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-scaling -o ../../build/marlowe-scaling
 nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-oracle -o ../../build/marlowe-oracle
