3aae538fa1696ca25f09af36f1aff68582a04ee9
Brian W Bush <code@functionally.io>
2023-02-20 10:13:56 -0700

    Retired marlowe history and discovery.

--- repos/build.sh
+++ repos/build.sh
@@ -4,16 +4,16 @@ set -ev
 
 cd marlowe-cardano
 
-nix build .#chainseekd -o ../../build/chainseekd
 nix build .#marlowe-chain-indexer -o ../../build/marlowe-chain-indexer
+nix build .#marlowe-chain-sync -o ../../build/marlowe-chain-sync
 nix build .#marlowe-indexer -o ../../build/marlowe-indexer
 nix build .#marlowe-sync -o ../../build/marlowe-sync
-nix build .#marlowe-history -o ../../build/marlowe-history
-nix build .#marlowe-discovery -o ../../build/marlowe-discovery
 nix build .#marlowe-tx -o ../../build/marlowe-tx
 nix build .#marlowe-rt -o ../../build/marlowe
 
+nix build .#marlowe-web-server -o ../../build/marlowe-web-server
+
+nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-pipe -o ../../build/marlowe-pipe
+nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-finder -o ../../build/marlowe-finder
 nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-scaling -o ../../build/marlowe-scaling
 nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-oracle -o ../../build/marlowe-oracle
-nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-finder -o ../../build/marlowe-finder
-nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-pipe -o ../../build/marlowe-pipe
