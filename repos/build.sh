2192c47dc2a664a8dedc4a67f1416af430c1c3df
Brian W Bush <code@functionally.io>
2023-07-17 10:36:53 -0600

    Updated to post-0.0.3 runtime.

--- repos/build.sh
+++ repos/build.sh
@@ -4,20 +4,22 @@ set -ev
 
 cd marlowe-cardano
 
-nix build .#marlowe-cli -o ../../build/marlowe-cli
+nix build .#marlowe-cli-exe-marlowe-cli-ghc8107 -o ../../build/marlowe-cli
 
-nix build .#marlowe-chain-indexer -o ../../build/marlowe-chain-indexer
-nix build .#marlowe-chain-sync -o ../../build/marlowe-chain-sync
-nix build .#marlowe-indexer -o ../../build/marlowe-indexer
-nix build .#marlowe-sync -o ../../build/marlowe-sync
-nix build .#marlowe-tx -o ../../build/marlowe-tx
-nix build .#marlowe-contract -o ../../build/marlowe-contract
-nix build .#marlowe-proxy -o ../../build/marlowe-proxy
+nix build .#marlowe-chain-sync-exe-marlowe-chain-indexer-ghc8107 -o ../../build/marlowe-chain-indexer
+nix build .#marlowe-chain-sync-exe-marlowe-chain-sync-ghc8107 -o ../../build/marlowe-chain-sync
+nix build .#marlowe-runtime-exe-marlowe-indexer-ghc8107 -o ../../build/marlowe-indexer
+nix build .#marlowe-runtime-exe-marlowe-sync-ghc8107 -o ../../build/marlowe-sync
+nix build .#marlowe-runtime-exe-marlowe-contract-ghc8107 -o ../../build/marlowe-contract
+nix build .#marlowe-runtime-exe-marlowe-tx-ghc8107 -o ../../build/marlowe-tx
+nix build .#marlowe-runtime-exe-marlowe-proxy-ghc8107 -o ../../build/marlowe-proxy
 
-nix build .#marlowe-runtime-cli -o ../../build/marlowe-runtime-cli
-nix build .#marlowe-web-server -o ../../build/marlowe-web-server
+nix build .#marlowe-runtime-cli-exe-marlowe-runtime-cli-ghc8107 -o ../../build/marlowe-runtime-cli
+nix build .#marlowe-runtime-web-exe-marlowe-web-server-ghc8107 -o ../../build/marlowe-web-server
 
-nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-pipe -o ../../build/marlowe-pipe
-nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-finder -o ../../build/marlowe-finder
-nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-scaling -o ../../build/marlowe-scaling
-nix build .#marlowe.haskell.packages.marlowe-apps.components.exes.marlowe-oracle -o ../../build/marlowe-oracle
+nix build .#marlowe-apps-exe-marlowe-pipe-ghc8107 -o ../../build/marlowe-pipe
+nix build .#marlowe-apps-exe-marlowe-finder-ghc8107 -o ../../build/marlowe-finder
+nix build .#marlowe-apps-exe-marlowe-scaling-ghc8107 -o ../../build/marlowe-scaling
+nix build .#marlowe-apps-exe-marlowe-oracle-ghc8107 -o ../../build/marlowe-oracle
+nix build .#marlowe-apps-exe-marlowe-streamer-ghc8107 -o ../../build/marlowe-streamer
+nix build .#marlowe-apps-exe-signing-service-ghc8107 -o ../../build/signing-service
